281f34c013010b29fe50e90fcdd1e74b8820e9c5
Nazar <fuhcen05@gmail.com>
2024-06-16 13:18:47 +0300

    Add files via upload

---
+++ lr20oop/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace lr20oop
+{
+    partial class Form1
+    {
+
+
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResult;
+
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(13, 13);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxA.TabIndex = 0;
+
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(13, 42);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxB.TabIndex = 1;
+
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(13, 71);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(100, 23);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Обчислити";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(13, 101);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 17);
+            this.labelResult.TabIndex = 3;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Розв'язувач лінійних рівнянь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ lr20oop/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lr20oop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBoxA.Text);
+                double b = double.Parse(textBoxB.Text);
+
+                if (a == 0)
+                {
+                    if (b == 0)
+                    {
+                        labelResult.Text = "Рівняння має безліч рішень";
+                    }
+                    else
+                    {
+                        labelResult.Text = "Рівняння не має рішень";
+                    }
+                }
+                else
+                {
+                    double x = -b / a;
+                    labelResult.Text = $"Корінь рівняння: x = {x}";
+                }
+            }
+            catch (FormatException)
+            {
+                labelResult.Text = "Помилка: введіть правильні числові значення для a та b.";
+            }
+            catch (Exception ex)
+            {
+                labelResult.Text = $"Невідома помилка: {ex.Message}";
+            }
+        }
+    }
+}
